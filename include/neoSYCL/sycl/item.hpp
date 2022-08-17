652b0bb917f39426bf49ddf5ba7258137a9fd67b
Hiroyuki Takizawa <takizawa@tohoku.ac.jp>
2022-08-17 16:10:47 +0900

    (add) item::get_range(), item::get_linear_id()

--- include/neoSYCL/sycl/item.hpp
+++ include/neoSYCL/sycl/item.hpp
@@ -44,6 +44,10 @@ struct item {
     return this->max_range;
   }
 
+  size_t get_range(int dimension) const {
+    return this->max_range[dimension];
+  }
+
   // only available if with_offset is true
   template <bool OFFSET = with_offset, typename = std::enable_if_t<OFFSET>>
   id<dimensions> get_offset() const {
@@ -54,6 +58,18 @@ struct item {
   template <bool OFFSET = with_offset, typename = std::enable_if_t<!OFFSET>>
   operator item<dimensions, true>() const {
     return item<dimensions, true>(this->max_range, this->data, this->data);
+  }
+
+  size_t get_linear_id() const {
+    size_t idx = 0;
+    if(dimensions > 0)
+      idx = data[0];
+    if(dimensions > 1)
+      idx = idx*max_range[0]+data[1];
+    if(dimensions > 2)
+      idx = idx*max_range[1]+data[2];
+    return idx;
+
   }
 
   DEFINE_ITEM_BY_VALUE_OP(item);
